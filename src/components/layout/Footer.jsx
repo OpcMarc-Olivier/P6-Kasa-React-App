e07d353945078d01e04539df3fffce1ee1ccc39f
OpcMarc-Olivier <mo.perrois@gmail.com>
2024-04-17 15:45:48 +0200

    ✨ F02: Add Footer

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Footer() {
+  return (
+   <footer>
+      <nav>
+        <img src="./LOGO.svg" alt="logo kasa application" />
+        <p>@all rights reserved</p>
+      </nav>
+    </footer>
+  )
+}
+
+export default Footer
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import {BrowserRouter} from 'react-router-dom';
 import HeaderNav from './components/layout/HeaderNav';
+import Footer from './components/layout/Footer';
 
 
 
@@ -13,6 +14,7 @@ root.render(
     <BrowserRouter>
       <HeaderNav />
       <App />
+      <Footer />
     </BrowserRouter>
   </React.StrictMode>
 );
