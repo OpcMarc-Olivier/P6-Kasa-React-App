d5fc7d9baf79393e523e2853d6ee8cfe54c11cce
OpcMarc-Olivier <mo.perrois@gmail.com>
2024-04-17 21:00:02 +0200

    ✨ F03: Add banner

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import "../../../styles/home.css";
 
 function Home() {
+  //state
+  //comportements
+  //render
   return (
-    <div>
-     <h1>Home</h1>
+    <div className="home-wrapper">
+      <div className="home-container">
+        <img
+          className="home-banner"
+          src="./imagesource1.jpg"
+          alt="falaises océan"
+        />
+        <div className="title-container">
+          <h2 className="home__title">Chez vous, partout et ailleurs</h2>
+        </div>
+      </div>
+      <div className="home-gallery">Gallery</div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
