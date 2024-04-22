9e0baec1c7d1bd030af9f4be04899e352c4c6fc8
OpcMarc-Olivier <mo.perrois@gmail.com>
2024-04-22 14:49:58 +0200

    ✨ F06 : ERROR > Create Error page

--- src/components/pages/error/Error.jsx
+++ src/components/pages/error/Error.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
+import "../../../styles/error.css";
+import { Link } from "react-router-dom";
 
 function Error() {
   return (
-    <div>
-     <h1>Error 404</h1>
+    <div className="error-page">
+      <h1 className="title-content">404</h1>
+      <p className="content">Oups! la page que vous demandez n'existe pas.</p>
+      <Link className="nav-link" to="/">
+        Retourner sur la page d'accueil
+      </Link>
     </div>
-  )
+  );
 }
 
-export default Error
+export default Error;
