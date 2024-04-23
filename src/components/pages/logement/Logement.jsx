1c3b5b59caff49d34d9ecfc15c9c68c47e3f4bfc
OpcMarc-Olivier <mo.perrois@gmail.com>
2024-04-23 21:23:55 +0200

    ✨ F07: Logement > Add useParams

--- src/components/pages/logement/Logement.jsx
+++ src/components/pages/logement/Logement.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+function Logement({ dataHome }) {
+  //state
+  // const [dataHome, setdataHome] = useState([]);
+  const [dataHomeId, setDataHomeId] = useState([]);
+  const { homeId } = useParams();
+  useEffect(() => {
+    console.log(dataHome);
+    const homeIdFound = dataHome.find((item, i) => {
+      return item.id === homeId;
+    });
+    console.log(homeIdFound);
+    setDataHomeId(homeIdFound);
+  });
 
-function Logement() {
+  // const myHome = ;
+
+  //comportements
+
+  //render
   return (
     <div>
-     <h1>Logement</h1>
+      <div></div>
+      <h1>{dataHomeId.title}</h1>
+      <p>{dataHomeId.description}</p>
+
+      <img src={dataHomeId.cover} alt="" />
     </div>
-  )
+  );
 }
 
-export default Logement
+export default Logement;
